fe276cea260a00905c56a4066aa108ecfca3683f
OmarAbdo <omargamer10a@gmail.com>
2025-12-13 23:41:23 +0200

    Migration DB after Deleteing GuidId from Review Entity

---
+++ TouristsRepository/Migrations/20251213203853_Remove GuidId From Review.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TouristsRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveGuidIdFromReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_GuideProfiles_GuideId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_GuideId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "GuideId",
+                table: "Reviews");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GuideId",
+                table: "Reviews",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_GuideId",
+                table: "Reviews",
+                column: "GuideId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_GuideProfiles_GuideId",
+                table: "Reviews",
+                column: "GuideId",
+                principalTable: "GuideProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
